d21f37603fee122df275d5f5f7161690838ab2d2
hannesi <hannesi@iki.fi>
2024-02-27 10:48:48 +0200

    Init logger on extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,11 @@
 import * as vscode from 'vscode';
 import * as init from "./init";
+import { Logger } from './utilities/logger';
 
 
 // This method is called when your extension is activated
 export function activate(ctx: vscode.ExtensionContext) {
+    Logger.init('TIDE Logs');
     init.registerCommands(ctx);
 }
 
--- src/init/commands.ts
+++ src/init/commands.ts
@@ -1,7 +1,11 @@
 import * as vscode from 'vscode';
+import { Logger } from '../utilities/logger';
 
 export function registerCommands(ctx: vscode.ExtensionContext) {
+    Logger.info('Registering commands.');
     ctx.subscriptions.push(
-        vscode.commands.registerCommand("tide.hello-world", () => vscode.window.setStatusBarMessage('Hello TIDE!'))
+        vscode.commands.registerCommand("tide.hello-world", () => {
+            vscode.window.setStatusBarMessage('Hello TIDE!');
+        })
     );
 }
