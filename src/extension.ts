f7f6078020d2c7941a3b3938834412f7a3cd3acf
hannesi <hannesi@iki.fi>
2024-03-06 16:16:08 +0200

    Outlining commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,16 @@
 import * as vscode from 'vscode';
 import * as init from "./init";
-import { Logger } from './utilities/logger';
+import Logger from './utilities/logger';
+import Tide from './api/tide';
 
 
 // This method is called when your extension is activated
 export function activate(ctx: vscode.ExtensionContext) {
     Logger.init('TIDE Logs');
+    Logger.show();
+    Tide.setCliPath('mock-cli');
     init.registerCommands(ctx);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
--- src/init/commands.ts
+++ src/init/commands.ts
@@ -1,11 +1,47 @@
 import * as vscode from 'vscode';
-import { Logger } from '../utilities/logger';
+import Logger from '../utilities/logger';
+import Tide from '../api/tide';
 
 export function registerCommands(ctx: vscode.ExtensionContext) {
     Logger.info('Registering commands.');
     ctx.subscriptions.push(
-        vscode.commands.registerCommand("tide.hello-world", () => {
+        vscode.commands.registerCommand('tide.hello-world', () => {
             vscode.window.setStatusBarMessage('Hello TIDE!');
         })
     );
+
+    ctx.subscriptions.push(
+        vscode.commands.registerCommand('tide.login', () => {
+            Tide.login();
+        })
+    );
+
+    ctx.subscriptions.push(
+        vscode.commands.registerCommand('tide.logout', () => {
+            Tide.logout();
+        })
+    );
+
+    // "commands": [
+    //   {
+    //     "command": "tide.login",
+    //     "title": "Login"
+    //   },
+    //   {
+    //     "command": "tide.logout",
+    //     "title": "Log out"
+    //   },
+    //   {
+    //     "command": "tide.list",
+    //     "title": "List Available Courses"
+    //   },
+    //   {
+    //     "command": "tide.pull",
+    //     "title": "Pull either all tasks for a course or a specific task."
+    //   },
+    //   {
+    //     "command": "tide.push",
+    //     "title": "Push either all tasks for a course or a specific task."
+    //   }
+    // ]
 }
