3afd4a67932d652cf07042e9b8c3925dd6e0f9fe
stefuuu <stella.palenius@gmail.com>
2024-03-16 20:41:09 +0200

    Dummy sidebar implemented

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,20 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import * as init from "./init";
-import Logger from './utilities/logger';
-import Tide from './api/tide';
-
+import Logger from "./utilities/logger";
+import Tide from "./api/tide";
+import { SidebarProvider } from "./panels/SidebarProvider";
 
 // This method is called when your extension is activated
 export function activate(ctx: vscode.ExtensionContext) {
-    Logger.init('TIDE Logs');
-    Logger.show();
-    Tide.setCliPath('mock-cli');
-    init.registerCommands(ctx);
+	Logger.init("TIDE Logs");
+	Logger.show();
+	Tide.setCliPath("mock-cli");
+	init.registerCommands(ctx);
+
+	// Creates and registers the side menu on the left
+	const sidebarProvider = new SidebarProvider(ctx.extensionUri);
+	ctx.subscriptions.push(vscode.window.registerWebviewViewProvider("tide-sidebar", sidebarProvider));
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    let isLoggedIn = false;
+  
+    function handleLogin() {
+      isLoggedIn = true;
+    }
+  
+    function handleLogout() {
+      isLoggedIn = false;
+    }
+</script>
+
+<style>
+  button {
+      text-align: left;
+  }
+  div {
+      color: turquoise;
+  }
+</style>
+  
+  {#if !isLoggedIn}
+    <div>Welcome to TIDE-extension! Start by logging in.</div>
+    <button on:click={handleLogin} style="background: none; border: none; cursor: pointer;">Login</button>
+  {:else}
+    <nav>
+      <ul style="padding: 0; margin: 0;">
+        <li style="list-style-type: none;">
+          <button on:click={() => { /* Handle My Courses click */ }} style="background: none; border: none; cursor: pointer;">My Courses</button>
+        </li>
+        <li style="list-style-type: none;">
+          <button on:click={() => { /* Handle Settings click */ }} style="background: none; border: none; cursor: pointer;">Settings</button>
+        </li>
+        <li style="list-style-type: none;">
+          <button on:click={handleLogout} style="background: none; border: none; cursor: pointer;">Logout</button>
+        </li>
+      </ul>
+    </nav>
+  {/if}
