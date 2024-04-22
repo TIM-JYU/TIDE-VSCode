126ca4b0e6e7a129d2e9a8b9f1d61ecd1619ba3d
stefuuu <stella.palenius@gmail.com>
2024-04-22 10:35:04 +0300

    Documentation for sidebar

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,76 +1,93 @@
 <script>
-    import { onMount } from 'svelte';
-    let showSidebarWelcome = false;
-    let coursesJson = [];
+  import { onMount } from 'svelte';
+  let showSidebarWelcome = false;
+  let isLoggedIn = false;
 
-    // Listen for messages from the extension
-    onMount(() => {
-        window.addEventListener('message', (event) => {
-            const message = event.data;
-            if (message && message.type === 'settingValue') {
-                showSidebarWelcome = message.value;
-            }
-        });
-    });
-    let isLoggedIn = false;
+  /**
+  * Listens to messages from the extension
+  */
+  onMount(() => {
+      window.addEventListener('message', (event) => {
+          const message = event.data;
+          if (message && message.type === 'settingValue') {
+              showSidebarWelcome = message.value;
+          }
+      });
+  });
 
-    function handleLogin() {
-      tsvscode.postMessage({
-        type: 'login',
-        value: ''
-      })
-      isLoggedIn = true;
-    }
-  
-    function handleLogout() {
-      tsvscode.postMessage({
-        type: 'logout',
-        value: ''
-      })
-      isLoggedIn = false;
-    }
+  /**
+   * Posts message for the extension for logging user in
+   */
+  function handleLogin() {
+    tsvscode.postMessage({
+      type: 'login',
+      value: ''
+    })
+    isLoggedIn = true;
+  }
+
+  /**
+   * Posts message fot extension for logging user out
+   */
+  function handleLogout() {
+    tsvscode.postMessage({
+      type: 'logout',
+      value: ''
+    })
+    isLoggedIn = false;
+  }
 </script>
+  
+{#if !isLoggedIn}
+  {#if showSidebarWelcome}
+    <div>Welcome to TIDE-extension! Start by logging in.</div>
+  {/if}
+  <button on:click={handleLogin}>Login</button>
+{:else}
+  <nav>
+    <ul class="nav-list">
+      <li>
+        <button on:click={() => { tsvscode.postMessage({
+          type: 'showCourses',
+          value: ''
+        })}}>My Courses</button>
+      </li>
+      <li>
+        <button on:click={() => { tsvscode.postMessage({
+          type: 'openSettings',
+          value: ''
+        })}}>Settings</button>
+      </li>
+      <li>
+        <button on:click={handleLogout}>Logout</button>
+      </li>
+    </ul>
+  </nav>
+{/if}
+
+<!-- 'Delete this later' -->
+<button on:click={() => {
+    tsvscode.postMessage({
+      type: 'showTaskPanel',
+      value: ''
+    })
+}}>Show task panel</button>
 
 <style>
-  button {
-      text-align: left;
-  }
   div {
-      color: turquoise;
+    color: turquoise;
+  }
+
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    text-align: left;
+  }
+
+  .nav-list {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
   }
 </style>
-  
-  {#if !isLoggedIn}
-    {#if showSidebarWelcome}
-      <div>Welcome to TIDE-extension! Start by logging in.</div>
-    {/if}
-    <button on:click={handleLogin} style="background: none; border: none; cursor: pointer;">Login</button>
-  {:else}
-    <nav>
-      <ul style="padding: 0; margin: 0;">
-        <li style="list-style-type: none;">
-          <button on:click={() => { tsvscode.postMessage({
-            type: 'showCourses',
-            value: ''
-          })}} style="background: none; border: none; cursor: pointer;">My Courses</button>
-        </li>
-        <li style="list-style-type: none;">
-          <button on:click={() => { tsvscode.postMessage({
-            type: 'openSettings',
-            value: ''
-          }) }} style="background: none; border: none; cursor: pointer;">Settings</button>
-        </li>
-        <li style="list-style-type: none;">
-          <button on:click={handleLogout} style="background: none; border: none; cursor: pointer;">Logout</button>
-        </li>
-      </ul>
-    </nav>
-  {/if}
-
-  <!-- 'Example of how sending a message from webview to VS Code api works' -->
-  <button on:click={() => {
-      tsvscode.postMessage({
-        type: 'showTaskPanel',
-        value: ''
-      })
-  }}>Show task panel</button>
