a81b8dd7215532b234e6e69c0047e5fe0509effb
stefuuu <stella.palenius@gmail.com>
2024-04-22 10:24:01 +0300

    repetitive code edited and Courses documented

--- webviews/components/CourseList.svelte
+++ webviews/components/CourseList.svelte
@@ -5,249 +5,246 @@
     export let status; // "active" or "hidden"
     export let courses; // array of courses
     export let moveCourse; // function to move a course between lists
-    export let downloadTaskSet;
-    export let openWorkspace;
-    export let isExpanded;
-    export let toggle;
-    export let toggleCourse;
+    export let downloadTaskSet; // function to download a task set
+    export let openWorkspace; // function to open a workspace for task set
+    export let isExpanded; // "true" or "false"
+    export let toggle; // function to hide or show "active" or "hidden" category for courses
+    export let toggleCourse; // function to collapse or expand the course information
   
+    /**
+     * Gets the opposite course status.
+     */
     function getOppositeStatus() {
       return status === "active" ? "hidden" : "active";
     }
 
+    /**
+     * Get's the text according to the course status.
+     */
     function statusText() {
         return status == "hidden" ? "Hidden" : "Active";
     }
   </script>
   
-  <button class="button-header" on:click={toggle}>
+  <button class="button-header" on:click={toggle(status)}>
     {statusText()} Courses
     <span class="arrow {!isExpanded ? 'down-arrow' : 'left-arrow'}">&#8250;</span>
   </button>
+
 
   {#if isExpanded}
     {#each courses as course (course.id)}
       <div class="course-box">
         <header>
           <p class="courseTitle">{course.name}</p>
-          <Menu>
-            <span slot='toggle'>&#8942;</span>
-            <MenuItem>
-              <a href="#?" on:click={() => moveCourse(course, getOppositeStatus())}>
-                Move to {getOppositeStatus()} courses
-              </a>
-            </MenuItem>
-          </Menu>
+            <Menu>
+              <span slot='toggle'>&#8942;</span>
+              <MenuItem>
+                <a href="#?" on:click={() => moveCourse(course, getOppositeStatus())}>
+                  Move to {getOppositeStatus()} courses
+                </a>
+              </MenuItem>
+            </Menu>
         </header>
-        <div>
-          <a class="link" href={"https://tim.jyu.fi/view/" + course.path}>Open material page</a>
-        </div>
-        <button
-          class="expand-collapse-button"
-          aria-expanded={course.expanded}
-          on:click={() => toggleCourse(course.id)}>
-            <span class="arrow {!course.expanded ? 'down-arrow' : 'up-arrow'}">&#9660;</span>
-        </button>
-        {#if course.expanded}
-                <div class="course-content">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Task set</th>
-                                <th>Number of exercises</th>
-                                <th>Points</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {#each course.task_docs as taskset}
-                                <tr>
-                                    <td>{taskset.name}</td>
-                                    <td>{taskset.tasks.length}</td>
-                                    <td>6/8</td>
-                                    <td><button class="download-button" on:click={() => downloadTaskSet(taskset.path)}>Download</button></td>
-                                    <td><button class="open-workspace" on:click={() => openWorkspace(taskset.name)}>Open in workspace</button></td>
-                                </tr>
-                            {/each}
-                        </tbody>
-                    </table>
-                </div>
-            {/if}
+      <div>
+        <a class="link" href={"https://tim.jyu.fi/view/" + course.path}>Open material page</a>
+      </div>
+      <button
+        class="expand-collapse-button"
+        aria-expanded={course.expanded}
+        on:click={() => toggleCourse(course.id)}>
+          <span class="arrow {!course.expanded ? 'down-arrow' : 'up-arrow'}">&#9660;</span>
+      </button>
+      {#if course.expanded}
+        <div class="course-content">
+          <table>
+            <thead>
+              <tr>
+                <th>Task set</th>
+                <th>Number of exercises</th>
+                <th>Points</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+              <tbody>
+              {#each course.task_docs as taskset}
+                <tr>
+                  <td>{taskset.name}</td>
+                  <td>{taskset.tasks.length}</td>
+                  <td>6/8</td>
+                  <td><button class="download-button" on:click={() => downloadTaskSet(taskset.path)}>Download</button></td>
+                  <td><button class="open-workspace" on:click={() => openWorkspace(taskset.name)}>Open in workspace</button></td>
+                </tr>
+              {/each}
+              </tbody>
+            </table>
+          </div>
+        {/if}
       </div>
     {/each}
   {/if}
 
 <style>
-    :global(body) {
-      margin-bottom: 2.5rem;
-    }
+  :global(body) {
+    margin-bottom: 2.5rem;
+  }
+
+  .button-header {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    border: none;
+    background: none;
+    font-size: 1.4rem;
+    font-weight: bold;
+    margin-top: 1.5rem;
+    padding: 0;
+    color: white;
+  }
+
+  .arrow {
+    margin-left: 8px;
+    transition: transform 0.3s ease;
+  }
+
+  .left-arrow {
+    transform: rotate(90deg);
+  }
+
+  .down-arrow {
+    transform: rotate(0deg);
+  }
+
+  .course-box {
+    position: relative;
+    background-color: #000000;
+    padding-bottom: 3.5rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    border-radius: 8px;
+    font-size: 1.4em;
+    max-width: 85%;
+    min-width: 24em;
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  .courseTitle {
+    margin-left: 1.5rem;
+    margin-top: 1.5rem;
+  }
+
+  .link {
+    margin-left: 1.5rem;
+    font-size: 0.9rem;
+    color: #007acc;
+  }
+
+  .link:hover {
+    text-decoration: underline;
+  }
+
+  .download-button, .open-workspace {
+    background-color: #007acc;
+    color: white;
+    border: none;
+    padding: 0.3rem 0.5rem;
+    border-radius: 4px;
+    font-size: small;
+    cursor: pointer;
+  }
+
+  .download-button:active, .open-workspace:active {
+    background:#004d80;
+  }
   
-    .button-header {
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-      border: none;
-      background: none;
-      font-size: 1.4rem;
-      font-weight: bold;
-      margin-top: 1.5rem;
-      padding: 0;
-      color: white;
-    }
-  
-    .arrow {
-      margin-left: 8px;
-      transition: transform 0.3s ease;
-    }
-  
-    .left-arrow {
-      transform: rotate(90deg);
-    }
-  
-    .down-arrow {
-      transform: rotate(0deg);
-    }
-  
-    .course-box {
-      position: relative;
-      background-color: #000000;
-      padding-bottom: 3.5rem;
-      margin-top: 1rem;
-      margin-bottom: 1rem;
-      border-radius: 8px;
-      font-size: 1.4em;
-      max-width: 85%;
-      min-width: 24em;
-      width: 100%;
-      box-sizing: border-box;
-    }
-  
-    .courseTitle {
-      margin-left: 1.5rem;
-      margin-top: 1.5rem;
-    }
-  
-    .link {
-      margin-left: 1.5rem;
-      font-size: 0.9rem;
-      color: #007acc;
-    }
-  
-    .link:hover {
-      text-decoration: underline;
-    }
-  
-    .download-button, .open-workspace {
-      background-color: #007acc;
-      color: white;
-      border: none;
-      padding: 0.3rem 0.5rem;
-      border-radius: 4px;
-      font-size: small;
-      cursor: pointer;
-    }
-  
-    .download-button:active, .open-workspace:active {
-      background:#004d80;
-    }
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+  }
     
-    *,
-      *::before,
-      *::after {
-        box-sizing: border-box;
-      }
-  
-      ul,
-      li {
-        margin: 0;
-          padding: 0;
-      }
-  
-    header {
-      display: flex;
-      justify-content: space-between;
-    }
-      
-      ul {
-          list-style-type: none;
-      }
-      
-      a {
-      color: inherit;
-      text-decoration: none;
-    }
-  
-    .expand-collapse-button {
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: white;
-      background-color: transparent;
-      border: none;
-      width: 36px;
-      height: 36px;
-      position: absolute;
-      bottom: 0rem;
-      left: 50%;
-      transform: translateX(-50%);
-    }
-  
-    .arrow {
-      transition: transform 0.5s ease;
-    }
-  
-    .down-arrow {
-      transform: rotate(0deg);
-    }
-  
-    .up-arrow {
-      transform: rotate(-180deg);
-    }
-  
-    .link::after {
-      content: '';
-      position: absolute;
-      bottom: 2rem;
-      left: 0;
-      width: 100%;
-      height: 1px;
-      background-color: gray;
-    }
-  
-    .course-content {
-      margin-top: 2rem;
-      max-width: 100%;
-      overflow-x: auto;
-      box-sizing: content-box;
-    }
-  
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      box-sizing: content-box;
-    }
-  
-    th, td {
-      border: none;
-      text-align: center;
-      padding: 8px;
-    }
-  
-    th {
-      background-color: black;
-      font-weight: normal;
-      font-size: smaller;
-    }
-  
-    tbody tr:nth-child(odd) {
-      background-color: #222222;
-    }
-  
-    tbody tr:nth-child(even) {
-      background-color: #444444;
-    }
-  
-  </style>
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  .expand-collapse-button {
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    background-color: transparent;
+    border: none;
+    width: 36px;
+    height: 36px;
+    position: absolute;
+    bottom: 0rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .arrow {
+    transition: transform 0.5s ease;
+  }
+
+  .down-arrow {
+    transform: rotate(0deg);
+  }
+
+  .up-arrow {
+    transform: rotate(-180deg);
+  }
+
+  .link::after {
+    content: '';
+    position: absolute;
+    bottom: 2rem;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: gray;
+  }
+
+  .course-content {
+    margin-top: 2rem;
+    max-width: 100%;
+    overflow-x: auto;
+    box-sizing: content-box;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    box-sizing: content-box;
+  }
+
+  th, td {
+    border: none;
+    text-align: center;
+    padding: 8px;
+  }
+
+  th {
+    background-color: black;
+    font-weight: normal;
+    font-size: smaller;
+  }
+
+  tbody tr:nth-child(odd) {
+    background-color: #222222;
+  }
+
+  tbody tr:nth-child(even) {
+    background-color: #444444;
+  }
+
+</style>
   
--- webviews/components/Courses.svelte
+++ webviews/components/Courses.svelte
@@ -1,19 +1,19 @@
 <script>
-  import Menu from './Menu.svelte'
-  import MenuItem from './MenuItem.svelte'
   import CourseList from './CourseList.svelte';
   import { onMount } from 'svelte';
 
   let downloadPath = '';
   let coursesJson = [];
+  let activeCoursesExpanded = true;
+  let hiddenCoursesExpanded = false;
 
   $: if (downloadPath== null) {
-    tsvscode.postMessage({
-    type: 'onError',
-    value: 'Folder for downloading tasks must be set'
-  })
+    directoryNotSet();
 	}
 
+  /**
+  * Listens for messages from CoursePanel.ts.
+  */
   onMount(() => {
     window.addEventListener('message', (event) => {
       const message = event.data;
@@ -30,66 +30,66 @@
     });
   });
 
-  let activeCoursesExpanded = true;
-  let hiddenCoursesExpanded = false;
-
-  function toggleActive() {
-    activeCoursesExpanded = !activeCoursesExpanded;
+  /**
+   * Toggles the visibility of courses based on their status.
+   * @param {string} status - Tells if the course status is "active" or "hidden".
+   */
+  function toggleVisibility(status) {
+    status === 'active' ? activeCoursesExpanded = !activeCoursesExpanded : hiddenCoursesExpanded = !hiddenCoursesExpanded;
   }
 
-  function toggleHidden() {
-    hiddenCoursesExpanded = !hiddenCoursesExpanded;
-  }
+  // Mock data for testing the webview
+  // let courses = [
+  //   {
+  //     "id": "1001",
+  //     "name": "Course1",
+  //     "task_docs": [
+  //       { "name": 1, "tasks": ["Exercise 1", "Exercise 2", "Exercise 3"] },
+  //       { "name": 2, "tasks": ["Exercise 4", "Exercise 5"] },
+  //       { "name": 3, "tasks": ["Exercise 6", "Exercise 7", "Exercise 8"] }
+  //     ],
+  //     "status": "active",
+  //     "expanded": false
+  //   },
+  //   {
+  //     "id": "1002",
+  //     "name": "Course2",
+  //     "task_docs": [
+  //       { "name": 1, "tasks": ["Exercise 1", "Exercise 2"] },
+  //       { "name": 2, "tasks": ["Exercise 3", "Exercise 4"] }
+  //     ],
+  //     "status": "active",
+  //     "expanded": false
+  //   },
+  //   {
+  //     "id": "1003",
+  //     "name": "Course3",
+  //     "task_docs": [
+  //       { "name": 1, "tasks": ["Exercise 1", "Exercise 2", "Exercise 3"] },
+  //       { "name": 2, "tasks": ["Exercise 4", "Exercise 5", "Exercise 6"] },
+  //       { "name": 3, "tasks": ["Exercise 7", "Exercise 8"] }
+  //     ],
+  //     "status": "hidden",
+  //     "expanded": false
+  //   }
+  // ];
 
-  let courses = [
-    {
-      "id": "1001",
-      "name": "Course1",
-      "exerciseWeeks": [
-        { "week": 1, "exercises": ["Exercise 1", "Exercise 2", "Exercise 3"] },
-        { "week": 2, "exercises": ["Exercise 4", "Exercise 5"] },
-        { "week": 3, "exercises": ["Exercise 6", "Exercise 7", "Exercise 8"] }
-      ],
-      "status": "active",
-      "expanded": false
-    },
-    {
-      "id": "1002",
-      "name": "Course2",
-      "exerciseWeeks": [
-        { "week": 1, "exercises": ["Exercise 1", "Exercise 2"] },
-        { "week": 2, "exercises": ["Exercise 3", "Exercise 4"] }
-      ],
-      "status": "active",
-      "expanded": false
-    },
-    {
-      "id": "1003",
-      "name": "Course3",
-      "exerciseWeeks": [
-        { "week": 1, "exercises": ["Exercise 1", "Exercise 2", "Exercise 3"] },
-        { "week": 2, "exercises": ["Exercise 4", "Exercise 5", "Exercise 6"] },
-        { "week": 3, "exercises": ["Exercise 7", "Exercise 8"] }
-      ],
-      "status": "hidden",
-      "expanded": false
-    }
-  ];
-
-  //TODO: keksi parempi
+  /**
+   * Updates the status of a course to a new status.
+   * @param {object} course - The course object to be updated.
+   * @param {string} status - The new status for the course.
+   */
   function moveCourse(course, status) {
     const index = coursesJson.findIndex(c => c.id === course.id);
     if (index !== -1) {
-        // Create a new array with the updated status
-        // coursesJson = [
-        //     ...coursesJson.slice(0, index),
-        //     { ...course, status },
-        //     ...coursesJson.slice(index + 1)
-        // ];
         coursesJson[index] = {...course, "status":status};
     }
   }
 
+  /**
+   * Toggles the expanded/collapsed state of a course information panel.
+   * @param {string} courseId - - The unique identifier of the course.
+   */
   function toggleCourse(courseId) {
     const courseIndex = coursesJson.findIndex(course => course.id === courseId);
     if (courseIndex !== -1) {
@@ -97,47 +97,61 @@
     }
   }
 
-  function downloadTaskSet(taskSetPath) {
-    // Check if downloadPath is set
-    if (!downloadPath) {
-      tsvscode.postMessage({
+  /**
+   * Notifies the extension that the user has not chosen a directory for downloading task sets,
+   * and posts an error message accordingly.
+   */
+  function directoryNotSet() {
+    tsvscode.postMessage({
         type: 'onError',
-        value: 'Folder for downloading tasks must be set'
+        value: 'Directory for downloading tasks must be set'
       });
+  }
+
+  /**
+   * Initiates the download of a task set identified by its path.
+   * @param {string} taskSetPath - The path of the task set to be downloaded.
+   */
+  function downloadTaskSet(taskSetPath) {
+    if (!downloadPath) {
+      directoryNotSet();
       return;
     }
     
-    // Send a message to the extension with the courseId and taskSetPath
     tsvscode.postMessage({
       type: 'downloadTaskSet',
       taskSetPath
     });
   }
 
+  /**
+   * Opens a workspace for the specified task set.
+   * @param {string} taskSetName - The name of the task set to open a workspace for.
+   */
   function openWorkspace(taskSetName) {
-    //Send a message to extension with taskset's name and downloadpath
     tsvscode.postMessage({
       type: 'openWorkspace',
       taskSetName,
       downloadPath
     });
-}
+  }
   
 </script>
+
 <h1>My Courses</h1>
 
-<p>Current download folder: {downloadPath}</p>
+<p>Current directory for downloading files: {downloadPath}</p>
 
 <button on:click={() => {
   tsvscode.postMessage({
     type: 'setPath',
     value: ''
   })
-}}>Set file download folder</button>
+}}>Set directory</button>
 
 <CourseList
   isExpanded={activeCoursesExpanded}
-  toggle={toggleActive}
+  toggle={toggleVisibility}
   toggleCourse={toggleCourse}
   status={"active"}
   courses={coursesJson.filter(c => c.status == "active")}
@@ -148,7 +162,7 @@
 
 <CourseList
   isExpanded={hiddenCoursesExpanded}
-  toggle={toggleHidden}
+  toggle={toggleVisibility}
   toggleCourse={toggleCourse}
   status={"hidden"}
   courses={coursesJson.filter(c => c.status == "hidden")}
