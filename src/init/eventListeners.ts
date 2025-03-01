40724634304029ec1f4c6849c259c82e098276dd
ReetuInkila <inkilareetu@gmail.com>
2025-03-01 13:38:18 +0200

    TTV-96 removed deprecated event listener

--- src/init/eventListeners.ts
+++ src/init/eventListeners.ts
@@ -10,40 +10,8 @@
 import * as vscode from 'vscode'
 import ExtensionStateManager from '../api/ExtensionStateManager'
 import Logger from '../utilities/logger'
-import UiController from '../ui/UiController'
-import { TaskPanelProvider } from '../ui/panels/TaskPanelProvider'
-import { editableAreaEventListener, isBycodeTaskFile } from '../editor/editableArea'
 
 export function registerEventListeners(ctx: vscode.ExtensionContext) {
-  let lastActiveEditor: vscode.TextEditor | undefined
-
-  /**
-   * Listens to changes of active text editor.
-   * Last active editor check ensures that if user closes taskpanel it won't open again
-   * when user activates or clicks the text document next to it right after closing.
-   * If they click another text document open, the task panel opens.
-   */
-  const textEditorListener = vscode.window.onDidChangeActiveTextEditor(async (editor) => {
-
-  TaskPanelProvider.updateCurrentActiveEditor(editor)
-
-    if (editor && editor !== lastActiveEditor) {
-      if (editor.document && editor.document.uri.scheme === 'file') {
-        lastActiveEditor = editor
-
-        //TaskPanelProvider.updateLastActiveEditor(lastActiveEditor)
-
-        UiController.showTaskPanel()
-
-        if (isBycodeTaskFile(editor.document)) {
-          editableAreaEventListener.activate(ctx, editor.document)
-        } else {
-          editableAreaEventListener.deactivate()
-        }
-      }
-    }
-  })
-  ctx.subscriptions.push(textEditorListener)
 
   /**
    * Listens to changes in configuration.
