87e55d29f78d44226bf62fb8e23536f7e56c1d8e
Reetu Inkilä <inkilareetu@gmail.com>
2025-03-28 07:22:30 +0200

    call task points after task submit

--- src/api/tide.ts
+++ src/api/tide.ts
@@ -10,7 +10,7 @@
 import * as cp from 'child_process'
 import Logger from '../utilities/logger'
 import * as vscode from 'vscode'
-import { Course, LoginData, Task, TaskCreationFeedback, TaskPoints, UserData } from '../common/types'
+import { Course, LoginData, Task, TaskCreationFeedback, TaskPoints, TimData, UserData } from '../common/types'
 import { parseCoursesFromJson } from '../utilities/parsers'
 import ExtensionStateManager from './ExtensionStateManager'
 import path from 'path'
@@ -171,8 +171,19 @@ export default class Tide {
   public static async submitTask(taskPath: string, callback: () => any) {
     this.runAndHandle(['submit', taskPath], (data: string) => {
       Logger.debug(data)
-      //Logger.debug(taskPath, path.basename(taskPath))
-      //this.getTaskPoints(taskPath, path.basename(taskPath), callback)
+      let pathSplit = taskPath.split(path.sep)
+      // id
+      let id = pathSplit.at(-2)
+      // task set name
+      let demo = pathSplit.at(-3)
+      if (demo && id) {
+        const timData : TimData | undefined = ExtensionStateManager.getTaskTimData(demo, id)
+        if (timData) {
+          this.getTaskPoints(timData.path, timData.ide_task_id, callback);
+        } else {
+          vscode.window.showErrorMessage('TimData is undefined or invalid.');
+        }
+      }
     })
   }
 
