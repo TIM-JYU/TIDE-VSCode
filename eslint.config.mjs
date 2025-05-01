791c4f5f7244a98689998e7faf9b97b7029bdef9
ReetuInkila <inkilareetu@gmail.com>
2025-05-01 12:52:09 +0300

    Update ESLint configuration and fix missing semicolon in CoursePanel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,29 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import { defineConfig, globalIgnores } from 'eslint/config' 
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
-export default defineConfig([globalIgnores(["**/out", "**/dist", "**/*.d.ts"]), {
+export default defineConfig([globalIgnores(['**/out', '**/dist', '**/*.d.ts']), {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+        '@typescript-eslint': typescriptEslint,
     },
 
     languageOptions: {
         parser: tsParser,
         ecmaVersion: 6,
-        sourceType: "module",
+        sourceType: 'module',
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
+        '@typescript-eslint/naming-convention': ['warn', {
+            selector: 'import',
+            format: ['camelCase', 'PascalCase'],
         }],
 
-        "@/semi": ["error", "never"],
-        curly: "error",
-        eqeqeq: "error",
-        "no-throw-literal": "warn",
-        semi: ["error", "never"],
-        quotes: ["error", "single"],
+        '@/semi': ['warn', 'never'],
+        curly: 'warn',
+        eqeqeq: 'warn',
+        'no-throw-literal': 'warn',
+        semi: ['warn', 'never'],
+        quotes: ['warn', 'single'],
     },
-}]);
+}])
